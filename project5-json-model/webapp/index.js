67f50ecba2a03c5d57e1aa7b4a64387db1daaa06
github <albi.g@live.com>
2026-03-12 10:54:46 +0100

    new project

---
+++ project5-json-model/webapp/index.js
@@ -0,0 +1,12 @@
+sap.ui.define(["sap/ui/core/mvc/XMLView"], (XMLView) => {
+  "use strict";
+
+  async function init() {
+    const oView = await XMLView.create({
+      viewName: "ui5.walkthrough.view.App",
+    });
+    oView.placeAt("content");
+  }
+
+    init();
+});
