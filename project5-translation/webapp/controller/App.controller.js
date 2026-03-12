9ffa618a393e3801d836c89d31e27f8a647a105e
github <albi.g@live.com>
2026-03-12 17:46:07 +0100

    translation

--- project5-translation/webapp/controller/App.controller.js
+++ project5-translation/webapp/controller/App.controller.js
@@ -3,8 +3,9 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
     "sap/m/MessageToast",
+    "sap/ui/model/resource/ResourceModel",
   ],
-  (Controller, JSONModel, MessageToast) => {
+  (Controller, JSONModel, MessageToast, ResourceModel) => {
     "use strict";
 
     return Controller.extend("ui5.walkthrough.controller.App", {
@@ -16,6 +17,12 @@ sap.ui.define(
         });
 
         this.getView().setModel(oModel);
+
+        const resourceModel = new ResourceModel({
+          resourceBundle: "ui5.walkthrough.i18n.i18n",
+        });
+
+        this.getView().setModel(resourceModel, "translation");
       },
 
       onPress() {
