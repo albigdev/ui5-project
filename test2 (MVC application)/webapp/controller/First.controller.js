ac5134ba48d6419dc6aa776a09bbf61296d60a3a
github <albi.g@live.com>
2026-03-16 18:36:52 +0100

    ...

--- test2 (MVC application)/webapp/controller/First.controller.js
+++ test2 (MVC application)/webapp/controller/First.controller.js
@@ -1,26 +1,45 @@
 sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
   "use strict";
-  return Controller.extend("gergely.albi.mvcapp.controller.First", {
-    onListItemPress: function (oEvent) {
+
+return class First extends Controller {
+    onListItemPress(oEvent) {
       const oAppView = sap.ui.getCore().byId("appView");
       const sPageId = oAppView.byId("detailPageId").getId();
       const oApp = oAppView.byId("app");
       const oPage = oApp.getPage(sPageId);
-
       const oContext = oEvent.getSource().getBindingContext();
+
       oPage.setBindingContext(oContext);
       oApp.to(sPageId);
+    }
 
-      /*const sPageId="appView--detailPageId";
-		 const oPage = oApp.getPage(sPageId);
-		 
-		 const oContext = oEvent.getSource().getBindingContext();
-		 oPage.setBindingContext(oContext);
-		 oApp.to(sPageId);*/
-    },
-    toLowerCase: function (sValue) {
+    toLowerCase(sValue) {
       const sString = String(sValue);
       return sString.toLowerCase();
-    },
-  });
+    }
+  }
+
+  // return Controller.extend("gergely.albi.mvcapp.controller.First", {
+  //   onListItemPress: function (oEvent) {
+  //     const oAppView = sap.ui.getCore().byId("appView");
+  //     const sPageId = oAppView.byId("detailPageId").getId();
+  //     const oApp = oAppView.byId("app");
+  //     const oPage = oApp.getPage(sPageId);
+
+  //     const oContext = oEvent.getSource().getBindingContext();
+  //     oPage.setBindingContext(oContext);
+  //     oApp.to(sPageId);
+
+  //     /*const sPageId="appView--detailPageId";
+	// 	 const oPage = oApp.getPage(sPageId);
+		 
+	// 	 const oContext = oEvent.getSource().getBindingContext();
+	// 	 oPage.setBindingContext(oContext);
+	// 	 oApp.to(sPageId);*/
+  //   },
+  //   toLowerCase: function (sValue) {
+  //     const sString = String(sValue);
+  //     return sString.toLowerCase();
+  //   },
+  // });
 });
