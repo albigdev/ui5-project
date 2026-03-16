bbeedb66ca0203571d92f71af21a597dc6b6251d
Gergely Albi <albi.g@live.com>
2026-03-16 15:25:43 +0000

    ...

--- test2 (MVC application)/webapp/controller/Second.controller.js
+++ test2 (MVC application)/webapp/controller/Second.controller.js
@@ -2,6 +2,8 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
   "use strict";
   return Controller.extend("gergely.albi.mvcapp.controller.Second", {
     onNavPress: function () {
+      const oAppView = sap.ui.getCore().byId("appView");
+      const oApp = oAppView.byId("app");
       oApp.back();
     },
   });
