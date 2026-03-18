c1d3be96da9e099299101db840ad4b780809f219
github <albi.g@live.com>
2026-03-18 14:46:24 +0100

    ...

---
+++ MVC app Binding mode/webapp/controller/First.controller.js
@@ -0,0 +1,55 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+  "use strict";
+
+  return class First extends Controller {
+    onListItemPress(oEvent) {
+      //const oRootView = this.getOwnerComponent().getRootControl(); // XML View App.view.xml
+
+      //const oAppViewPages = oAppView.getPages(); //We get the pages of the XML view App.view.xml
+      //const firstView = oAppViewPages.find((oPage) => oPage.getId().includes("initialPage"))
+      //const firstViewId = firstView.getId(); //We get the id of the first page in the XML view App.view.xml
+
+      //const oApp = oRootView.byId("app"); // App control in the XML view App.view.xml
+      //const sPageId = oPage.getId();
+
+      const oContext = oEvent.getSource().getBindingContext(); //We get the data of the item we have pressed on the list
+
+      const sID = oContext.getProperty("ID");
+
+      const oRouter = this.getOwnerComponent().getRouter();
+
+      oRouter.navTo("detail", {
+        ID: sID,
+      });
+    }
+
+    toLowerCase(sValue) {
+      const sString = String(sValue);
+      return sString.toLowerCase();
+    }
+  };
+
+  // return Controller.extend("gergely.albi.mvcapp.controller.First", {
+  //   onListItemPress: function (oEvent) {
+  //     const oAppView = sap.ui.getCore().byId("appView");
+  //     const sPageId = oAppView.byId("detailPageId").getId();
+  //     const oApp = oAppView.byId("app");
+  //     const oPage = oApp.getPage(sPageId);
+
+  //     const oContext = oEvent.getSource().getBindingContext();
+  //     oPage.setBindingContext(oContext);
+  //     oApp.to(sPageId);
+
+  //     /*const sPageId="appView--detailPageId";
+  // 	 const oPage = oApp.getPage(sPageId);
+
+  // 	 const oContext = oEvent.getSource().getBindingContext();
+  // 	 oPage.setBindingContext(oContext);
+  // 	 oApp.to(sPageId);*/
+  //   },
+  //   toLowerCase: function (sValue) {
+  //     const sString = String(sValue);
+  //     return sString.toLowerCase();
+  //   },
+  // });
+});
---
+++ MVC app Binding mode/webapp/controller/Second.controller.js
@@ -0,0 +1,34 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+  "use strict";
+
+  return class Second extends Controller {
+    onInit() {
+      const oRouter = this.getOwnerComponent().getRouter();
+      oRouter
+        .getRoute("detail")
+        .attachPatternMatched(this._onRouteMatched, this); //If the route activates, we call this function
+    }
+
+    onNavPress() {
+      const oRouter = this.getOwnerComponent().getRouter();
+      oRouter.navTo("master");
+    }
+
+    _onRouteMatched(oEvent) {
+      const sId = oEvent.getParameter("arguments").ID;
+
+      const oView = this.getView();
+      oView.bindElement({
+        path: "/SweetsSupplier/" + sId,
+      });
+    }
+  };
+
+  // return Controller.extend("gergely.albi.mvcapp.controller.Second", {
+  //   onNavPress: function () {
+  //     const oAppView = sap.ui.getCore().byId("appView");
+  //     const oApp = oAppView.byId("app");
+  //     oApp.back();
+  //   },
+  // });
+});
