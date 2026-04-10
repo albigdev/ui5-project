bf0a0cf2f9caf008834e57e397085d0e8313adc9
github <albi.g@live.com>
2026-04-10 13:37:19 +0200

    ...

--- MVC app + Odata/webapp/controller/Third.controller.js
+++ MVC app + Odata/webapp/controller/Third.controller.js
@@ -1,5 +1,57 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+  ],
+  function (Controller, JSONModel, Filter, FilterOperator) {
+    "use strict";
 
-  return class Third extends Controller {};
-});
+    return class Third extends Controller {
+      onInit() {
+        const oViewModel = new JSONModel({
+          count: 0,
+        });
+        this.getView().setModel(oViewModel, "viewModel");
+
+        const oTable = this.byId("productsTable");
+        oTable.attachUpdateFinished(() => {
+          const iCount = oTable.getItems().length;
+          this.getView().getModel("viewModel").setProperty("/count", iCount);
+        });
+
+        const oRouter = this.getOwnerComponent().getRouter();
+        oRouter
+          .getRoute("product")
+          .attachPatternMatched(this._onRouteMatched, this);
+      }
+
+      onNavPress() {
+        const oRouter = this.getOwnerComponent().getRouter();
+        oRouter.navTo("detail", {
+          ID: this._sSupplierId,
+        });
+      }
+
+      _onRouteMatched(oEvent) {
+        const sId = oEvent.getParameter("arguments").ID;
+        const iSupplierId = parseInt(sId, 10);
+        this._sSupplierId = sId;
+
+        this.getView().bindElement({
+          path: `/Products`,
+        });
+
+        const oTable = this.byId("productsTable");
+        const oBinding = oTable.getBinding("items");
+
+        if (oBinding) {
+          oBinding.filter([
+            new Filter("SupplierID", FilterOperator.EQ, iSupplierId),
+          ]);
+        }
+      }
+    };
+  },
+);
