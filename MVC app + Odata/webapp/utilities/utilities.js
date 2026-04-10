52ecbd3fce083000555ae843bd4753f58d76b071
github <albi.g@live.com>
2026-04-10 18:12:16 +0200

    ...

--- MVC app + Odata/webapp/utilities/utilities.js
+++ MVC app + Odata/webapp/utilities/utilities.js
@@ -5,10 +5,16 @@ sap.ui.define([], function () {
     toLowerCase: function (sValue) {
       return String(sValue).toLowerCase();
     },
-    unitState: function (number) {
-      if (number > 20) return "Success";
-      if (number <= 20) return "Warning";
-      if (number <= 10) return "Error";
+    unitState: function (value) {
+      const number = Number(value);
+
+      if (number <= 10) {
+        return "Error";
+      }
+      if (number <= 20) {
+        return "Warning";
+      }
+      return "Success";
     },
   };
 });
